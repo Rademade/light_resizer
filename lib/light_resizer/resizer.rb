61b212acc8c12191fcaf4e5222d62bcedab25449
Sergey Kuzhavskiy <kuzh@rademade.com>
2015-10-09 15:07:42 +0300

    small refactoring

--- lib/light_resizer/resizer.rb
+++ lib/light_resizer/resizer.rb
@@ -8,51 +8,64 @@ module LightResizer
     end
 
     def process
-      full_path = "#{@public_path}#{@options}"
-      return if File.exists? full_path
-      read_image
+      return if image_exists?
       resize_or_crop
-      create_directory File.dirname(full_path)
-      save_image full_path
+      create_directory
+      save_image
     end
     
     protected
 
-    def read_image
-      @image = ::Magick::Image.read("#{@public_path}#{@options[:directory]}/#{@options[:image]}").first
+    def image_exists?
+      File.exists? full_path
     end
 
     def resize_or_crop
-      width = @options[:width].to_i
-      height = @options[:height].to_i
-      check_allowed_dimension width, height
-      @options[:crop].nil? ? resize(width, height) : crop(width, height)
+      check_allowed_dimension
+      @options[:crop].nil? ? resize : crop
     end
 
-    def check_allowed_dimension(width, height)
+    def check_allowed_dimension
       unless LightResizer.configuration.allowed_dimensions.empty? || LightResizer.configuration.allowed_dimensions.include?([width, height])
         raise DimensionNotFound, "Dimension #{width}x#{height} is not allowed"
       end
     end
 
-    def resize(width, height)
-      @image.resize_to_fill! width, height
+    def crop
+      image.resize_to_fill! width, height
     end
 
-    def crop(width, height)
-      @image.resize_to_fit! width, height
-      @image.background_color = 'Transparent'
+    def resize
+      image.resize_to_fit! width, height
+      image.background_color = 'Transparent'
       # does not work with simple gravity
-      @image = @image.extent width, height, (@image.columns - width) / 2, (@image.rows - height) / 2
+      @image = image.extent width, height, (image.columns - width) / 2, (image.rows - height) / 2
     end
     
-    def create_directory(directory_name)
+    def create_directory
+      directory_name = File.dirname(full_path)
       FileUtils.mkpath(directory_name) unless Dir.exist? directory_name
     end
     
-    def save_image(full_path)
-      @image.write full_path
+    def save_image
+      image.write full_path
       ImageOptimizer.new(full_path, quiet: true).optimize
+    end
+
+    def image
+      @image ||= ::Magick::Image.read("#{@public_path}#{@options[:directory]}/#{@options[:image]}").first
+    end
+
+    def full_path
+      @full_path ||= "#{@public_path}#{@options}"
+    end
+
+    def width
+      @width ||= @options[:width].to_i
+    end
+
+    def height
+      @height ||= @options[:height].to_i
     end
 
   end
